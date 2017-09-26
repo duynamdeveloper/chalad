3ca3ef0a70cdb5ddf940f480f2d0243a10587ce2
duynamdeveloper <duynam.dev223@gmail.com>
2017-09-26 19:01:22 +0700

    adjust stock movements, custom reason,

--- public/dist/js/pages/stock-movements/stock-movements.js
+++ public/dist/js/pages/stock-movements/stock-movements.js
@@ -1,49 +1,42 @@
+$(document).ready(function() {
 
+    $(".btnEdit").click(function() {
+        var id = $(this).attr('data-stock-movement-id');
+        $.ajax({
+            url: SITE_URL + '/stock/movement/get-by-id',
+            type: 'get',
+            data: {
+                'id': id,
+            },
+            success: function(data) {
+                console.log(data);
+                $("#hiddenInpStockMoveId").val(data.id);
+                $("#selStockId").val(data.stock_id);
+                $("#inpQty").val(data.quantity);
+                $("#inpReason").val(data.reason);
+                $("#selType").val(data.type);
+                $("#stockMovementForm").attr('action', SITE_URL + '/stock/movement/edit');
+                $("#stockMovementModal").modal('show');
+            }
+        });
+    });
+    $(".btnRemove").click(function() {
+        var id = $(this).attr('data-stock-movement-id');
+        var check = confirm("Are you sure");
+        if (check) {
+            $.ajax({
+                url: SITE_URL + '/stock/movement/delete',
+                type: 'post',
+                data: {
+                    'id': id
+                },
+                success: function(data) {
+                    if (data.state) {
+                        location.reload();
+                    }
+                }
+            });
+        }
 
-$(document).ready(function(){
-	$("#selReason").change(function(event) {
-		if($(this).val()=="create_new"){
-			$("#createNewReasonGroup").show();
-		}else{
-			$("#createNewReasonGroup").hide();
-		}
-	});
-	$(".btnEdit").click(function(){
-		var id = $(this).attr('data-stock-movement-id');
-		$.ajax({
-			url: SITE_URL+'/stock/movement/get-by-id',
-			type:'get',
-			data:{
-				'id': id,
-			},
-			success: function(data){
-				console.log(data);
-				$("#hiddenInpStockMoveId").val(data.id);
-				$("#selStockId").val(data.stock_id);
-				$("#inpQty").val(data.quantity);
-				$("#selReason").val(data.reason_id);
-				$("#stockMovementForm").attr('action',SITE_URL+'/stock/movement/edit');
-				$("#stockMovementModal").modal('show');
-			}
-		});
-	});
-	$(".btnRemove").click(function(){
-		var id = $(this).attr('data-stock-movement-id');
-		var check = confirm("Are you sure");
-		if(check){
-		$.ajax({
-			url: SITE_URL+'/stock/movement/delete',
-			type:'post',
-			data:{
-				'id':id
-			},
-			success: function(data){
-				if(data.state){
-					location.reload();
-				}
-			}
-		});
-		}
-
-	});
+    });
 });
